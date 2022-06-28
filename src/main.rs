7438c83aca7d5ba7eab7cafc3068207588c83ded
Yuval Adam <_@yuv.al>
2022-06-28 09:33:17 +0300

    Add rand call

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,20 @@
 use futures::executor::block_on;
+use rand::prelude::*;
+
+trait SimpleFuture {
+    type Output;
+    fn poll(&mut self, wake: fn()) -> Poll<Self::Output>;
+}
+
+enum Poll<T> {
+    Ready(T),
+    Pending,
+}
 
 async fn hello_world() {
-    println!("hello, world!");
+    let mut rng = rand::thread_rng();
+    let y: f64 = rng.gen();
+    println!("hello, world! {}", y);
 }
 
 fn main() {
